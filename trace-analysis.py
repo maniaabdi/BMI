33472849c95ee8a2bf5cb846a4ca3770064c57b2
maniaabdi <abdi.ma@ccs.neu.edu>
2017-12-01 08:17:10 -0500

    first 1 commit

--- trace-analysis.py
+++ trace-analysis.py
@@ -1,2 +1,22 @@
 # 1. open file vm_table 
 # 2. get maximum vm count as the refrence number
+
+import csv
+ 
+ def readMyFile(filename):
+         dates = []
+             scores = []
+              
+                  with open(filename) as csvDataFile:
+                              csvReader = csv.reader(csvDataFile)
+                                      for row in csvReader:
+                                                      dates.append(row[0])
+                                                                  scores.append(row[1])
+                                                                   
+                                                                       return dates, scores
+                                                                    
+                                                                    
+                                                                   dates,scores = readMyFile('file.csv')
+                                                                    
+                                                                    print(dates)
+                                                                    print(scores)
